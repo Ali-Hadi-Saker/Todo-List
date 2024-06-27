77dccac9317ff77719c3f510647e5d0922f010b5
Ali-Hadi-Saker <alihady530@gmail.com>
2024-06-27 21:25:43 +0300

    saving task in local storage

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,7 +3,9 @@ const add_task_btn = document.querySelector('button')
 const new_task_input = document.getElementsByClassName('task_input_field')[0]
 const user_task = document.getElementsByClassName('task_responsable_name')[0]
 const toda_date = document.getElementsByClassName('date')[0]
+const tasks_list = []
 add_task_btn.addEventListener('click', function(){
+    addTask(new_task_input)
     let new_task_todo = document.createElement('li')
     let new_user_task = document.createElement('p')
     new_task_todo.innerText = new_task_input.value
@@ -12,8 +14,12 @@ add_task_btn.addEventListener('click', function(){
     todo_card.appendChild(new_user_task)
     new_task_input.value = ''
     user_task.value = ''
-    console.log(new_task_todo)
+    console.log(tasks_list)
 })
+function addTask(new_task_input){
+    tasks_list.push(new_task_input.value)
+    localStorage.setItem('task', JSON.stringify(tasks_list))
+}
 function displayDate(){
     let date = new Date()
     date = date.toString().split(" ")
