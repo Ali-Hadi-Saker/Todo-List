ab4b7411e0b4b6ee87137bcf6807837f052d60eb
Ali-Hadi-Saker <alihady530@gmail.com>
2024-06-27 18:57:52 +0300

    editing input field for new task

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,10 @@
+let todo_card = document.getElementsByClassName('todo task-actions')
 let add_task_btn = document.querySelector('button')
 add_task_btn.addEventListener('click', function(){
+    let new_task_todo = document.createElement('input')
+    new_task_todo.type = 'text'
+    new_task_todo.placeholder = 'Enter new task'
+    new_task_todo.className = 'new_task_todo'
+    todo_card[0].appendChild(new_task_todo)
     console.log("hello world")
 })
