0c4f141139d078d3a1656ab78068474a3847ec5a
Ali-Hadi-Saker <alihady530@gmail.com>
2024-06-27 20:46:53 +0300

    cleaning input field after showing task and user

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,9 +6,10 @@ add_task_btn.addEventListener('click', function(){
     let new_task_todo = document.createElement('li')
     let new_user_task = document.createElement('p')
     new_task_todo.innerText = new_task_input.value
-    new_user_task.innerText = user_task.value
+    new_user_task.innerText = `user: ${user_task.value}`
     todo_card.appendChild(new_task_todo)
     todo_card.appendChild(new_user_task)
-
+    new_task_input.value = ''
+    user_task.value = ''
     console.log(new_task_todo)
 })
