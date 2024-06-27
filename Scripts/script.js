750dbfc60922931bcc0b91dfc9f3462d7f7dffa4
Ali-Hadi-Saker <alihady530@gmail.com>
2024-06-27 20:03:31 +0300

    adding new task user to the up comming task card

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,14 @@
 const todo_card = document.getElementsByClassName('todo task-actions')[0]
 const add_task_btn = document.querySelector('button')
 const new_task_input = document.getElementsByClassName('task_input_field')[0]
+const user_task = document.getElementsByClassName('task_responsable_name')[0]
 add_task_btn.addEventListener('click', function(){
     let new_task_todo = document.createElement('li')
+    let new_user_task = document.createElement('p')
     new_task_todo.innerText = new_task_input.value
+    new_user_task.innerText = user_task.value
     todo_card.appendChild(new_task_todo)
+    todo_card.appendChild(new_user_task)
+
     console.log(new_task_todo)
 })
