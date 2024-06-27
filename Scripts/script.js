0a308c43bc828b2e38ceda2f136615db92f14429
Ali-Hadi-Saker <alihady530@gmail.com>
2024-06-27 19:58:50 +0300

    adding new field for task resonsable

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,7 +2,7 @@ const todo_card = document.getElementsByClassName('todo task-actions')[0]
 const add_task_btn = document.querySelector('button')
 const new_task_input = document.getElementsByClassName('task_input_field')[0]
 add_task_btn.addEventListener('click', function(){
-    let new_task_todo = document.createElement('p')
+    let new_task_todo = document.createElement('li')
     new_task_todo.innerText = new_task_input.value
     todo_card.appendChild(new_task_todo)
     console.log(new_task_todo)
