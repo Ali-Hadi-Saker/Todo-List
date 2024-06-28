1e124df6b31d8e69cc3fb1b05f831b712299a1d2
Ali-Hadi-Saker <alihady530@gmail.com>
2024-06-28 13:33:01 +0300

    having problem when deleting from post due tasks

--- Scripts/script.js
+++ Scripts/script.js
@@ -8,11 +8,13 @@ const today_date = document.getElementsByClassName('date')[0]
 const task_time = document.getElementsByClassName('task_input_time')[0]
 let tasks_list = JSON.parse(localStorage.getItem('task')) || []
 let users_list = JSON.parse(localStorage.getItem('user')) || []
+let task_time_list = []
 let finished_tasks = []
 let finished_task_users = []
-let task_time_list = []
+let finished_task_time_list = []
 let post_due_task = []
 let post_due_task_time = []
+let post_due_task_user = []
 add_task_btn.addEventListener('click', function(){
     if(new_task_input.value === ''){
         alert("Enter your task")
@@ -21,14 +23,15 @@ add_task_btn.addEventListener('click', function(){
     showTask()    
     new_task_input.value = ''
     user_task.value = ''
+    task_time.value = ''
     console.log(tasks_list)
     }
     
 })
 function addTask(new_task_input, user_task, task_time){
 
-    tasks_list.push(new_task_input.value)
-    users_list.push(user_task.value)
+    tasks_list.push(new_task_input.value.trim())
+    users_list.push(user_task.value.trim())
     task_time_list.push(task_time.value)
     //adding new task into my tasks list
     localStorage.setItem('task', JSON.stringify(tasks_list))
@@ -42,16 +45,6 @@ function showTask(){
     let missed_task = ''
     let now = new Date()
     for(let i = 0 ; i < tasks_list.length ; i++){
-        for(let j = 0; j < post_due_task.length; j++){
-            missed_task +=  `<div class="item">
-            <div class="input_controller ">
-                <textarea class="new_task ">${tasks_list[i]} \nuser: ${users_list[i]} \ntime: ${task_time_list[i]}</textarea>                       
-            <div class="edit_controller">
-                <i class="fa-solid fa-xmark delete_btn"></i>
-            </div>
-        </div>                    
-        </div>`
-        }
         let task_time = new Date(task_time_list[i])
         if( now > task_time){
             missed_task +=  `<div class="item">
@@ -63,7 +56,6 @@ function showTask(){
         </div>                    
         </div>`
         moveToPostDue(i)
-        //move task from pending storage to post due storage
         }else{
             new_task += `<div class="item">
             <div class="input_controller ">
@@ -94,14 +86,18 @@ function showTask(){
 }
 function moveToPostDue(i){
     post_due_task.push(tasks_list[i])
+    post_due_task_user.push(users_list[i])
     post_due_task_time.push(task_time_list[i])
     tasks_list.splice(i, 1)
+    users_list.splice(i, 1)
     task_time_list.splice(i, 1)
     localStorage.setItem('task', JSON.stringify(tasks_list))
     localStorage.setItem('user', JSON.stringify(users_list))
+    localStorage.setItem('time', JSON.stringify(task_time_list))
     localStorage.setItem('post_due_task', JSON.stringify(post_due_task))
+    localStorage.setItem('post_due_user', JSON.stringify(post_due_task_user))
     localStorage.setItem('post_due_time', JSON.stringify(post_due_task_time))
-    showTask()
+    
 
 }
 function finishedTask(){
@@ -126,7 +122,10 @@ function deleteTask(){
     //access all delete btn 
     let delete_btn = document.querySelectorAll('.delete_btn')
     delete_btn.forEach((db, i) =>{
-        db.addEventListener('click', ()=>{deleteItem(i)})
+        db.addEventListener('click', ()=>{
+            let status = db.closest('.item')
+            console.log(status)
+            deleteItem(i)})
         //call delete item function for the corresponding task
     });
 }
@@ -137,12 +136,17 @@ function deleteItem(i){
     finished_tasks.splice(i, 1)
     finished_task_users.splice(i,1)
     task_time_list.splice(i,1)
+    post_due_task.splice(i, 1)
+    post_due_task_time.splice(i, 1)
     localStorage.setItem('task', JSON.stringify(tasks_list))
     //updating local storage    
     localStorage.setItem('user', JSON.stringify(users_list))
     localStorage.setItem('finished_task', JSON.stringify(finished_tasks));
     localStorage.setItem('finished_user', JSON.stringify(finished_task_users));
     localStorage.setItem('time', JSON.stringify(task_time_list))
+    localStorage.setItem('post_due_task', JSON.stringify(post_due_task));
+    localStorage.setItem('post_due_time', JSON.stringify(post_due_task_time))
+
     showTask()
 }
 function displayDate(){
